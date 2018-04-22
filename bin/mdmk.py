2fbffb7259be28cccf00d9b503daca0a5d2e2484
bwangel <bwangel.me@gmail.com>
2018-04-22 18:03:53 +0800

    Update on $(date "+%Y-%m-%d %h:%M:%S")

--- bin/mdmk.py
+++ bin/mdmk.py
@@ -9,9 +9,18 @@ def main():
     file, ext = os.path.splitext(source)
     target = "{}.out".format(file)
     compiler = "gcc" if ext == ".c" else "g++"
+    std = "c99" if ext == ".c" else "gnu++11"
     makefile = """{target}:{source}
-	{compiler} -o {target} -g {source}""" \
-        .format(**{'target': target, 'source': source, 'compiler': compiler})
+	{compiler} -o {target} -I ~/third_part/include/ -L ~/third_part/lib/ -lgtest -lpthread -g {source} --std={std}
+
+.PHONY: clean
+clean:
+	rm -f {target}""".format(**{
+            'target': target,
+            'source': source,
+            'compiler': compiler,
+            'std': std,
+        })
 
     with open("Makefile", "w+") as fd:
         fd.write(makefile)
