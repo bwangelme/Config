b21c9c08a61ab5cc2298866fe12e2aeb188b596a
bwangelme <bwangel.me@gmail.com>
2020-02-24 10:51:31 +0800

    Feat: move to dotzsh

--- Notebook/start_notebook.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-NOTEBOOK_CMD="/home/xff/.virtualenvs/jupyter/bin/jupyter-notebook --config=/home/xff/.jupyter/jupyter_notebook_config.py"
-exec $NOTEBOOK_CMD
--- docker/README.md
+++
@@ -1,4 +0,0 @@
-说明
-====
-
-这个配置文件为Docker快键键的配置文件，存放位置为`~/.docker/config.json`
